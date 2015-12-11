e607386fcabe89fbf95b0ee41f989c4c6579ac4f
Arkadij Kuzhel <akuzhel@atypicalbrands.com>
2015-12-11 18:16:58 +0200

    Configure application

--- laraTest/config/app.php
+++ laraTest/config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => 'http://laratest.localhost.com',
 
     /*
     |--------------------------------------------------------------------------
@@ -144,7 +144,10 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /**
+         * Doctrine
+         */
+//        LaravelDoctrine\ORM\DoctrineServiceProvider::class,
     ],
 
     /*
@@ -194,6 +197,12 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        /**
+         * Doctrine
+         */
+//        'EntityManager' => LaravelDoctrine\ORM\Facades\EntityManager::class,
+//        'Registry' => LaravelDoctrine\ORM\Facades\Registry::class,
+//        'Doctrine' => LaravelDoctrine\ORM\Facades\Doctrine::class,
     ],
 
 ];

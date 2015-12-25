cb055b01f53aae8611737e63d1bb1d7e4dbf9b64
Arkadij Kuzhel <akuzhel@atypicalbrands.com>
2015-12-25 15:20:44 +0200

    PHPUnit - test ArticlesController actions: list, create

---
+++ laraTest/tests/Doctrination/ArticleControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Doctrination;
+
+use TestCase;
+
+class ArticleControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testList()
+    {
+        $this->visit('/articles')
+            ->see('Articles')
+            ->click('create new')
+            ->seePageIs('/articles/create');
+    }
+
+    public function testCreate()
+    {
+        $this->visit('/articles/create')
+            ->see('Write a New Article')
+            ->see('Back to List')
+            ->click('Back to List')
+            ->seePageIs('/articles');
+
+        $articleData = [
+            'title' => 'Test New Article Create',
+            'body'  => 'A falsis, repressor noster diatria. Est dexter capio, cesaris.',
+        ];
+
+        $this->visit('/articles/create')
+            ->type($articleData['title'], 'title')
+            ->type($articleData['body'], 'body')
+            ->press('Add Article');
+
+        $this->seePageIsRegex('/articles/\d+')
+            ->see($articleData['title'])
+            ->see($articleData['body']);
+    }
+
+    public function seePageIsRegex($uri)
+    {
+        $this->assertPageLoaded($uri);
+
+        $this->assertRegExp("#{$uri}#", $this->currentUri, "Did not land on expected page [{$uri}].\n");
+
+        return $this;
+    }
+}
